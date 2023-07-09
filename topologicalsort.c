cb94bf44301bf722764bcee7fcd4d19f3b93abbf
Sagar C Mannannavar <118201159+sagarcm13@users.noreply.github.com>
2023-07-10 00:00:50 +0530

    Updated topologicalsort.c

--- topologicalsort.c
+++ topologicalsort.c
@@ -1,43 +1,59 @@
 #include <stdio.h>
-void main()
-{
-    int i, j, k, n, a[10][10], indeg[10], visited[10], count = 0;
-    printf("Enter the no of vertices:\n");
-    scanf("%d", &n);
-    printf("Enter the adjacency matrix:\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("Enter row %d\n", i + 1);
-        for (j = 0; j < n; j++)
-            scanf("%d", &a[i][j]);
-    }
-    for (i = 0; i < n; i++)
-    {
-        indeg[i] = 0;
+#define size 100
+int visited[size];
+int graph[size][size];
+int n;
+int stack[size];
+int top = -1;
+
+void initialize() {
+    int i, j;
+    for (i = 0; i < size; i++) {
         visited[i] = 0;
-    }
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
-            indeg[i] = indeg[i] + a[j][i];
-    printf("\nThe topological order is:");
-    while (count < n)
-    {
-        for (k = 0; k < n; k++)
-        {
-            if ((indeg[k] == 0) && (visited[k] == 0))
-            {
-                printf("%d ", (k + 1));
-                visited[k] = 1;
-            }
-            if(indeg[k]>0)
-            {
-                for (i = 0; i < n; i++)
-                {
-                    if (a[i][k] == 1)
-                        indeg[k]--;
-                }
-            }
+        for (j = 0; j < size; j++) {
+            graph[i][j] = 0;
         }
-        count++;
     }
+}
+
+void addEdge(int src, int dest) {
+    graph[src][dest] = 1;
+}
+
+void DFS(int vertex) {
+    int i;
+    visited[vertex] = 1;    
+    for (i = 0; i < n; i++) {
+        if (graph[vertex][i] == 1 && visited[i] == 0) {
+            DFS(i);
+        }
+    }    
+    stack[++top] = vertex;
+}
+
+void topologicalSort() {
+    int i;
+    for (i = 0; i < n; i++) {
+        if (visited[i] == 0) {
+            DFS(i);
+        }
+    }
+    printf("Topological order: ");
+    while (top >= 0) {
+        printf("%d ", stack[top--]);
+    }
+    printf("\n");
+}
+void main() {
+    int i, j;
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+    initialize();
+    printf("Enter the adjacency matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+    topologicalSort();
 }
