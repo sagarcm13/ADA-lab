f265a146eda35cccf4b50ed89ee9cd0fee093610
Sagar C Mannannavar <118201159+sagarcm13@users.noreply.github.com>
2023-07-17 14:51:41 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -1,54 +1,62 @@
-#include <stdio.h>
-void swap(int *i, int *j)
-{
-    int t = *i;
-    *i = *j;
-    *j = t;
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void swap(int *x,int *y){
+    int t=*x;
+    *x=*y;
+    *y=t;
 }
-int position(int *a, int l, int h)
-{
-    int i = l, j = h + 1, p = a[l];
+int partion(int a[],int l,int h){
+    int i=l,j=h+1;
+    int pivot =a[l];
     while (1)
     {
         do
         {
             i++;
-        } while (p >= a[i]);
+        } while (a[i]<pivot);
         do
         {
             j--;
-        } while (p < a[j]);
-        if (i < j)
+        } while (a[j]>pivot);
+        if (i<j)
         {
-            swap(&a[i], &a[j]);
+            swap(&a[i],&a[j]);
         }
-        if (i>=j)
+        if (i>j)
         {
+            swap(&a[l],&a[j]);
             break;
         }
-        
     }
-    if (i > j)
-    {
-        swap(&a[l], &a[j]);
-    }
-    return j;
+     return j;
+    
 }
-void qsort(int *a, int l, int h)
-{
-    if (l < h)
+void quicksort(int a[],int l,int h){
+    if (l<h)
     {
-        int m = position(a, l, h);
-        qsort(a, l, m - 1);
-        qsort(a, m + 1, h);
+        int m=partion(a, l, h);
+        quicksort(a,l,m-1);
+        quicksort(a,m+1,h);
     }
+    
 }
-void main()
-{
-    int a[5] = {15, 23, 13, 44, 65};
-    qsort(a, 0, 4);
-    for (int i = 0; i < 5; i++)
+void main(){
+    int n;
+    printf("enter the no of elements\n");
+    scanf("%d",&n);
+    clock_t st,end;
+    int a[n];
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ", a[i]);
+        a[i]=rand()%500;
     }
+    st=clock();
+    quicksort(a, 0, n-1);
+    end=clock();
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ",a[i]);
+    }        
+    printf("\nTime taken to %d elements %lf ",n,(((double)(end-st))/CLOCKS_PER_SEC));
 }
